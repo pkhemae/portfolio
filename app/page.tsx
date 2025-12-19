9a5ab4d0e060a788980c0fba62b9dc06282b6d0c
pkhemae <parckhemara@gmail.com>
2025-12-19 20:52:33 +0100

    ui: add lamp section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { TracingBeamSection } from "@/components/TracingBeam";
+import { Lamp } from "@/components/Lamp";
 
 export default function Home() {
   return (
     <div>
       <TracingBeamSection />
+      <Lamp />
     </div>
   );
 }
