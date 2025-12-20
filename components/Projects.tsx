3c13bc5822780f01bcb9bcbc44268a06cbcc342d
pkhemae <parckhemara@gmail.com>
2025-12-20 12:23:13 +0100

    ui: update portfolio project card

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,7 +12,7 @@ export function Projects() {
                 <div className="w-full h-full flex items-center justify-center bg-transparent">
                     <PinContainer
                         title="khemaraparc.fr"
-                        href="#"
+                        href="https://github.com/pkhemae/portfolio"
                     >
                         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                             <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
@@ -22,6 +22,13 @@ export function Projects() {
                                 <span className="text-slate-500 ">
                                     Le site de portfolio sur lequel vous vous trouvez actuellement. Il me permet de présenter les projets sur lesquels j'ai pu travailler.
                                 </span>
+                            </div>
+                            <div className="flex flex-1 w-full rounded-lg mt-4 items-center justify-center min-h-0 overflow-hidden">
+                                <img
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiNrNbDnlknOdcF7yivFB4rWCcpnuHfPwikA&s"
+                                    alt="GitHub"
+                                    className="object-contain w-full h-full"
+                                />
                             </div>
                         </div>
                     </PinContainer>
